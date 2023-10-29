0bdc1bd8d9add82bddbe05f8c05bbb5c44afb95f
unknown <azul.alfaro.fernandez@gmail.com>
2023-10-28 20:51:16 -0600

    Proyecto v1

---
+++ app/src/main/java/com/azul/ejercicio1/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.azul.ejercicio1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.azul.ejercicio1.ui.theme.Ejercicio1Theme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            setContentView(R.layout.activity_main)
+                }
+            }
+ }
+
+
